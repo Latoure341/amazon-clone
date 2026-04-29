7d57292f5eabefbbdeb3a217b862d032fd231a4a
Eric <tukamothupi@gmail.com>
2026-04-29 14:12:13 +0200

    Added foter and fixed the UI of the landing page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
-import Products from '../Products/Products'
+import { PiAmazonLogo } from 'react-icons/pi'
+import { GiSouthAfricaFlag } from "react-icons/gi";
+import { IoIosMenu } from "react-icons/io";
 
 const Home = () => {
   return (
     <>
       <section className="absolute top-10 left-0 text-center h-screen w-screen z-[-100] overflow-x-hidden">
-        <img src='https://m.media-amazon.com/images/I/71KOWlHgg-L._SX3000_.jpg' alt='background' className="w-full h-full" />
+        <img src='https://m.media-amazon.com/images/I/71KOWlHgg-L._SX3000_.jpg' alt='background' className="w-screen h-full" />
+      </section>
+
+      <section className='bg-[#232f3e] flex items-center gap-3 text-white w-4/5'>
+
+        <span className='flex items-center gap-1 text-white font-bold p-2 cursor-pointer hover:border-1'>
+          <IoIosMenu className='text-white'/>
+          All
+        </span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Customer Service</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Today's Deals</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Keep Shopping For</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Everyday Essentials</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Best Sellers</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Shop Mzansi</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Gift Cards</span>
+        <span className='text-sm font-bold p-2 cursor-pointer hover:border-1'>Sell</span>
       </section>
 
       {/* First section */}
@@ -214,8 +232,87 @@ const Home = () => {
         </span>
       </section>
 
-      <footer className='bg-blue-900 mt-20 p-5 '>
-        <p>Footer here</p>
+      <footer className='mt-20 p-0 flex flex-col items-center'>
+        <span className='bg-[#37475a] text-white py-2 px-4 w-full text-center cursor-pointer'>
+          Back to top
+        </span>
+
+        <div className='flex flex-wrap gap-30 bg-[#232f3e] pt-10 w-full justify-center'>
+
+          <span className='flex flex-col'>
+            <h3 className='text-white font-bold mb-2'>Get to Know Us</h3>
+            <ul className='text-white text-sm flex flex-col gap-2'>
+              <li>Careers</li>
+              <li>Legal Notice</li>
+              <li>Welcome to Amazon.co.za</li>
+            </ul>
+          </span>
+          <span className='flex flex-col'>
+            <h3 className='text-white font-bold mb-2'>Make Money with Us</h3>
+            <ul className='text-white text-sm flex flex-col gap-2'>
+              <li>Advertise Your Products</li>
+              <li>Sell on Amazon</li>
+              <li>Supply to Amazon</li>
+            </ul>
+          </span>
+          <span className='flex flex-col'>
+            <h3 className='text-white font-bold mb-2'>Amazon Payment Methods</h3>
+            <ul className='text-white text-sm flex flex-col gap-2'>
+              <li>Payment Methods Help</li>
+            </ul>
+          </span>
+          <span className='flex flex-col'>
+            <h3 className='text-white font-bold mb-2'>Let Us Help You</h3>
+            <ul className='text-white text-sm flex flex-col gap-2'>
+              <li>Track Packages or View Orders</li>
+              <li>Shipping & Delivery</li>
+              <li>Returns and Replacements</li>
+              <li>Recalls and Product Safety Alerts</li>
+              <li>Customer Service</li>
+              <li>Amazon Mobile App</li>
+            </ul>
+          </span>
+
+          <span className='my-5 mx-0 p-5 flex justify-center items-center gap-3 border-t-1 border-gray-500 w-full'>
+            <PiAmazonLogo className='text-white font-bold'/>
+            <button className='text-white py-1 px-3 rounded border-1 flex items-center gap-1' type='button'>
+              <GiSouthAfricaFlag/>
+              South Africa
+              </button>
+          </span>
+        </div>
+
+        <div className='flex flex-col gap-5 bg-[#131a22] text-white py-2 px-4 w-full text-center'>
+          <div className='flex justify-center items-center gap-20 mt-5'>
+            <span className='text-start'>
+              <h3 className='text-white text-sm font-bold'>Amazon Advertising</h3>
+              <a className='text-gray-300 text-xs'>Find, attract and engage customers</a>
+            </span>
+            <span className='text-start'>
+              <h3 className='text-white text-sm font-bold'>Amazon Advertising</h3>
+              <a className='text-gray-300 text-xs'>Find, attract and engage customers</a>
+            </span>
+            <span className='text-start'>
+              <h3 className='text-white text-sm font-bold'>Amazon Advertising</h3>
+              <a className='text-gray-300 text-xs'>Find, attract and engage customers</a>
+            </span>
+            <span className='text-start'>
+              <h3 className='text-white text-sm font-bold'>Amazon Advertising</h3>
+              <a className='text-gray-300 text-xs'>Find, attract and engage customers</a>
+            </span>
+          </div>
+
+          <div className='flex justify-center items-center p-5 w-full'>
+            <ul className='text-white text-xs flex gap-3 flex-wrap justify-center w-3/5'>
+              <li>Conditions of Use & Sale</li>
+              <li>Privacy Notice</li>
+              <li>Cookies Notice</li>
+              <li>Legal Notice</li>
+              <li>Interst-based ad Notice</li>
+              <li>&copy; 1996-2023, Amazon.com, Inc. or its affiliates</li>
+            </ul>
+          </div>
+        </div>
       </footer>
     </>
   )
